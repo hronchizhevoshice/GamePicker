e2883c08a34f5f20fd7354ffd8dcaa97c740709b
hronchizhevoshice <hronchizhevoshice@gmail.com>
2026-05-03 09:59:20 +0700

    add color theme

--- app/src/main/java/com/example/gamepicker/presentation/theme/Color.kt
+++ app/src/main/java/com/example/gamepicker/presentation/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.gamepicker.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Black = Color(0xFF000000)
+val Teal = Color(0xFF008080)
+val Beige = Color(0xFFF5F5DC)
+val White = Color(0xFFFFFFFF)
+val DarkTeal = Color(0xFF004040)
+val LightBeige = Color(0xFFFFF8E7)
+val Gray80 = Color(0xFFCCCCCC)
