fef4fe9a5df76afcf4e49b61cd4ba0237682ba13
hronchizhevoshice <hronchizhevoshice@gmail.com>
2026-05-03 10:11:17 +0700

    apply background color and verify color scheme

--- app/src/main/java/com/example/gamepicker/MainActivity.kt
+++ app/src/main/java/com/example/gamepicker/MainActivity.kt
@@ -5,21 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.gamepicker.presentation.theme.GamePickerTheme
+import com.example.gamepicker.presentation.theme.Teal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GamePickerTheme {
-                Box(
+                Surface(
                     modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
+                    color = Teal
                 ) {
-                    Text("GamePicker")
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "GamePicker",
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
                 }
             }
         }
