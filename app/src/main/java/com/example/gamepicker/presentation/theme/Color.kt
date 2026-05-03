8cfbee0db0639403d3314771b5c4388c87757a9c
hronchizhevoshice <hronchizhevoshice@gmail.com>
2026-05-03 09:55:22 +0700

    create clean architecture package structure

--- app/src/main/java/com/example/gamepicker/presentation/theme/Color.kt
+++ app/src/main/java/com/example/gamepicker/presentation/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.gamepicker.ui.theme
+package com.example.gamepicker.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/gamepicker/presentation/theme/Type.kt
+++ app/src/main/java/com/example/gamepicker/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.gamepicker.ui.theme
+package com.example.gamepicker.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
